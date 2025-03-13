aec2ec0b2a2dfc0a3d3a5374c8ad9722174a1d48
Antonio Purificato <80911741+antoniopurificato@users.noreply.github.com>
2025-03-13 09:59:22 +0100

    Implemented Informer and Performer.

--- src/predictor/train_predictor.py
+++ src/predictor/train_predictor.py
@@ -18,28 +18,32 @@ from src.predictor.temporal_transformer import TransformerPredictor
 from src.predictor.prepare_data import ArchitectureDataset
 
 
-if __name__ == '__main__':
-    
+import deepspeed
+
+deepspeed.init_distributed(dist_backend=None, distributed_port=29480)
+
+if __name__ == "__main__":
+
     with open("src/configs/predictor_config.yaml", "r") as config_file:
         config = yaml.safe_load(config_file)
 
-    seed = config['seed']
+    seed = config["seed"]
     seed_everything(seed)
 
     start = time.time()
 
-    # Load the datasets and models dictionaries  
+    # Load the datasets and models dictionaries
     try:
-        with open(load_yaml_exp_folder()[0], 'rb') as handle:
+        with open(load_yaml_exp_folder()[0], "rb") as handle:
             datasets_to_features = pickle.load(handle)
-        with open(load_yaml_exp_folder()[1], 'rb') as handle:
+        with open(load_yaml_exp_folder()[1], "rb") as handle:
             models_to_features = pickle.load(handle)
     except FileNotFoundError:
         print("You do not have the data! I'll download them")
         download_data()
-        with open(load_yaml_exp_folder()[0], 'rb') as handle:
+        with open(load_yaml_exp_folder()[0], "rb") as handle:
             datasets_to_features = pickle.load(handle)
-        with open(load_yaml_exp_folder()[1], 'rb') as handle:
+        with open(load_yaml_exp_folder()[1], "rb") as handle:
             models_to_features = pickle.load(handle)
 
     # Initialize the full dataset
@@ -47,18 +51,22 @@ if __name__ == '__main__':
 
     # Filter out the test datasets
     cv_data, nlp_data, recommendation_data = full_dataset.filter_test_data(
-        dataset_names=['cifar10', 'rotten_tomatoes', 'foursquare-tky']
+        dataset_names=["cifar10", "rotten_tomatoes", "foursquare-tky"]
     )
 
     # Combine all test datasets into a single dictionary for exclusion
     all_test_data = {**cv_data, **nlp_data, **recommendation_data}
 
     # Exclude all test dataset keys from the training/validation set
-    remaining_data = {key: value for key, value in full_dataset.valid_data.items() if key not in all_test_data}
+    remaining_data = {
+        key: value
+        for key, value in full_dataset.valid_data.items()
+        if key not in all_test_data
+    }
     full_dataset.valid_data = remaining_data
-    
+
     # Split the full dataset into training and validation sets
-    val_split_ratio = 0.20 # Proportion of data for validation
+    val_split_ratio = 0.20  # Proportion of data for validation
     train_size = int(len(full_dataset) * (1 - val_split_ratio))
     val_size = len(full_dataset) - train_size
     train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
@@ -66,34 +74,41 @@ if __name__ == '__main__':
     # Initialize dataloaders
     train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False)
-    
-    num_features = 1670 
+
+    num_features = 1670
     seq_len = 400
     num_targets = 2
 
     checkpoint_callback = ModelCheckpoint(
-    monitor="val_loss",           
-    dirpath="ckpt",       
-    filename=str(seed),  
-    save_top_k=1,                
-    mode="min", 
-    verbose=True
-)
+        monitor="val_loss",
+        dirpath="ckpt",
+        filename=str(seed),
+        save_top_k=1,
+        mode="min",
+        verbose=True,
+    )
 
     # Define the EarlyStopping callback
     early_stopping_callback = EarlyStopping(
-    monitor="val_loss",  # Metric to monitor
-    patience=12,          # Number of epochs with no improvement after which training will stop
-    verbose=True,        # Print early stopping message
-    mode="min"           # "min" because we want to minimize the validation loss
-)
+        monitor="val_loss",  # Metric to monitor
+        patience=12,  # Number of epochs with no improvement after which training will stop
+        verbose=True,  # Print early stopping message
+        mode="min",  # "min" because we want to minimize the validation loss
+    )
     # Initialize the model
-    model = TransformerPredictor(num_features=num_features, seq_len=seq_len, num_targets=num_targets)
+    model = TransformerPredictor(
+        num_features=num_features, seq_len=seq_len, num_targets=num_targets
+    )
 
     # Initialize the trainer
-    trainer= pl.Trainer(max_epochs=300, log_every_n_steps=10, callbacks=[early_stopping_callback, checkpoint_callback], logger=True)
+    trainer = pl.Trainer(
+        max_epochs=300,
+        log_every_n_steps=10,
+        callbacks=[early_stopping_callback, checkpoint_callback],
+        logger=True,
+    )
     # Train the model
-    trainer.fit(model,train_dataloader, val_dataloader)
+    trainer.fit(model, train_dataloader, val_dataloader)
 
     end = time.time()
 
